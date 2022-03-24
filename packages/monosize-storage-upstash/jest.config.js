b9c39b53208f645d929c9d1e9b6ad985d2fa2bf3
Oleksandr Fediashov <olfedias@microsoft.com>
2022-03-24 15:10:37 +0100

    chore: add storage package

---
+++ packages/monosize-storage-upstash/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'monosize-storage-upstash',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/monosize-storage-upstash',
+};
