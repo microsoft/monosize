1eb5d67e2b6886f38f9e5b8a6930e8adbc412e97
Oleksandr Fediashov <olfedias@microsoft.com>
2022-11-25 11:57:00 +0100

    fix lint issue

--- packages/monosize/src/__mocks__/find-up.ts
+++ packages/monosize/src/__mocks__/find-up.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const findUp: typeof import('find-up').findUp = async (configName, options) => {
-  const filePath = path.resolve(options?.cwd, configName as string);
+  const filePath = path.resolve((options?.cwd as string | undefined) || '', configName as string);
 
   if (fs.existsSync(filePath)) {
     return filePath;
